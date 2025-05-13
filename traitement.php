9ca336f142f7fecab9eee789632e37b72ed8121c
kisutoo <malk-om@outlook.fr>
2025-05-13 16:46:29 +0200

    appli

---
+++ traitement.php
@@ -0,0 +1,25 @@
+<?php // Plusieurs variables prédéfinies en PHP sont "superglobales", ce qui signifie qu'elles sont disponibles quel que soit le contexte du script.
+    session_start(); // Démarrer une session sur le serveur pour l'utilisateur courant ou récupérer la session de cet utilisateur s'il en avait déjà une
+
+    if(isset($_POST['submit'])) // Vérification de l'existance de la clé "submit"(attribut "name" du bouton <input type="submit" name="submit" du formulaire) dans le tableau "$_POST"
+    {                           // Vrai seulement si la requête POST transmet bien une clé "submit" au serveur
+
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT); // Différents filtres pour empêcher la saisie de mauvaises informations (injection de code etc)
+        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+        if($name && $price && $qtt) // Vérification si les filtres ont bien fonctionné et que n p q ne sont pas null ou vides
+        {
+            $product = [ // récupération des infos du produit
+                "name" => $name,
+                "price" => $price,
+                "qtt" => $qtt,
+                "total" => $price*$qtt
+            ];
+
+            $_SESSION['products'][] = $product;
+        }
+    }
+
+    header("Location:index.php"); // Dans le cas ou le if du dessus n'est pas vrai,  effectue une redirection grâce à la fonction header() (code 302) --> (code 200 sur la page cible)
+?>
